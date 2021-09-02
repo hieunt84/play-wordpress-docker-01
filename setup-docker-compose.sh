84812971247aae148fdcdcb9f27e435acac26f4e
HieuNT <hieunt9@gmail.com>
2021-09-02 10:20:09 +0700

    update 2Sep2021

---
+++ setup-docker-compose.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Install docker-compose
+sudo curl -sL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
